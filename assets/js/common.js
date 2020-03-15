d2d7a492c0ce10f318484b5bcc3a7b8b0e774938
diamond5601406 <jimbaran5601406@gmail.com>
2020-03-15 15:18:38 +0900

    add: 手書きネーム、サイトロゴ追加

---
+++ assets/js/common.js
@@ -0,0 +1,25 @@
+// ハンバーガーボタンが押すとハンバーガーボタンにactiveクラスが追加される
+// ハンバーガーボタンがactiveクラスを持ってるか否かでハンバーガーメニューにactiveクラスをつけるかどうか条件分岐
+$(function() {
+　$('.smp-nav-trigger').click(function() {
+　　$(this).toggleClass('active');
+
+	　if ($(this).hasClass('active')) {
+	　　$('.smp-nav-list').addClass('active');　 //クラスを付与
+	　} else {
+	　　$('.smp-nav-list').removeClass('active'); //クラスを外す
+	　}
+　});
+
+	//ハンバーガーメニュー内の">"クリック時にハンバーガーメニュー閉じる
+	$('.smp-nav-btn-inactive').click(function() {
+		$('.smp-nav-trigger').toggleClass('active');
+
+		if ($('.smp-nav-trigger').hasClass('active')) {
+	　　$('.smp-nav-list').addClass('active');　 //クラスを付与
+	　} else {
+	　　$('.smp-nav-list').removeClass('active'); //クラスを外す
+	　}
+
+	});
+});
