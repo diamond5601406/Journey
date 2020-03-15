3ccb6f9052dcb90fcd8c4b05c6b9ef02ff89cf5b
diamond5601406 <jimbaran5601406@gmail.com>
2020-03-15 19:00:26 +0900

    modified: ハンバーガーメニュー表示時のスクロールロック

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,24 +1,40 @@
+// ナビゲーションをフェードイン
+$(function() {
+    $(document).ready(function() {
+        $('.nav').css({
+            'opacity': '1',
+            'transition': 'all 1s ease-in 4s'
+        });
+    });
+});
+
 // ハンバーガーボタンが押すとハンバーガーボタンにactiveクラスが追加される
 // ハンバーガーボタンがactiveクラスを持ってるか否かでハンバーガーメニューにactiveクラスをつけるかどうか条件分岐
 $(function() {
-　$('.smp-nav-trigger').click(function() {
-　　$(this).toggleClass('active');
+    $('.smp-nav-trigger').click(function() {
 
-	　if ($(this).hasClass('active')) {
-	　　$('.smp-nav-list').addClass('active');　 //クラスを付与
-	　} else {
-	　　$('.smp-nav-list').removeClass('active'); //クラスを外す
-	　}
-　});
+    	const targetElement = document.querySelector('.nav');
 
-	//ハンバーガーメニュー内の">"クリック時にハンバーガーメニュー閉じる
-	$('.smp-nav-btn-inactive').click(function() {
-		$('.smp-nav-trigger').toggleClass('active');
+        $(this).toggleClass('active');
 
-		if ($('.smp-nav-trigger').hasClass('active')) {
-	　　$('.smp-nav-list').addClass('active');　 //クラスを付与
-	　} else {
-	　　$('.smp-nav-list').removeClass('active'); //クラスを外す
-	　}
-	});
+        if ($(this).hasClass('active')) {
+            $('.smp-nav-list').addClass('active'); //クラスを付与
+            $()
+        } else {
+            $('.smp-nav-list').removeClass('active'); //クラスを外す
+        }
+    });
+
+    //ハンバーガーメニュー内の">"クリック時にハンバーガーメニュー閉じる
+    $('.smp-nav-btn-inactive').click(function() {
+        $('.smp-nav-trigger').toggleClass('active');
+
+        if ($('.smp-nav-trigger').hasClass('active')) {
+            $('.smp-nav-list').addClass('active'); //クラスを付与
+            bodyScrollLock.disableBodyScroll(targetElement);
+        } else {
+            $('.smp-nav-list').removeClass('active'); //クラスを外す
+            bodyScrollLock.clearAllBodyScrollLocks();
+        }
+    });
 });
