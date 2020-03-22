fe363106cf53810135713377c1f65bd52f05461c
diamond5601406 <jimbaran5601406@gmail.com>
2020-03-22 10:24:35 +0900

    modified: header.phpとnavigation.phpのテンプレートフォルダへの移行

--- index.php
+++ index.php
@@ -1,38 +1,10 @@
-<?php require_once("./header.php"); ?>
+<?php require_once("./template/header.php"); ?>
 
 <body>
     <div class="container-fluid">
         <div class="row no-gutters">
-            <div class="col-md-2">
-                <div class="nav">
-                    <div class="nav-logo">
-                        <a href="/"><img src="assets/img/site-logo.png"></a>
-                    </div>
-                    <ul class="nav-list">
-                        <li><a href="" class="home">Home</a></li>
-                        <li><a href="#Profile" class="profile">Profile</a></li>
-                        <li><a href="#Works" class="works">Works</a></li>
-                        <li><a href="#Gallery" class="gallery">Gallery</a></li>
-                        <li><a href="#Contact" class="contact">Contact</a></li>
-                    </ul>
-                    <!-- ハンバーガーメニュ―内部 768以上で非表示-->
-                    <ul class="smp-nav-list">
-                        <div class="smp-nav-btn-inactive">></div>
-                        <li><a href="" class="home">Home</a></li>
-                        <li><a href="#Profile" class="profile">Profile</a></li>
-                        <li><a href="#Works" class="works">Works</a></li>
-                        <li><a href="#Gallery" class="gallery">Gallery</a></li>
-                        <li><a href="#Contact" class="contact">Contact</a></li>
-                    </ul>
-                    <!-- ハンバーガーボタン(ハンバーガーメニュー起爆剤) 687以下で表示 -->
-                    <div class="smp-nav-trigger" href="#">
-                        <!-- ←ハンバーガーボタン -->
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
+
+            <?php require("./template/navigation.php"); ?>
 
             <div class="col-md-10 smp-home-main">
                 <div class="main">
--- template/header.php
+++ template/header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/lib/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Cinzel&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="shortcut icon" href="assets/img/favicon.jpg">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="assets/lib/masonry.pkgd.min.js"></script>
