56a6504876098c4355dc21f18d3ee5f67a02aea2
webdeveloperkei <jimbaran5601406@gmail.com>
2020-03-28 12:13:32 +0900

    modified: Profileページ(スマホ)のレイアウト修正

--- profile.php
+++ profile.php
@@ -11,15 +11,23 @@
       			<div class="col-12 profile-area-wrapper">
       				<div class="profile-area">
       					<h1>PROFILE</h1>
+                <div class="profile-area-icon">
+                  <a href="./contact.php">
+                    <img src="./assets/img/mail-icon.png" alt="メールアイコン">
+                  </a>
+                  <a href="https://twitter.com/WebDeveloperKei" target="blank">
+                    <img src="./assets/img/twitter-icon.png" alt="ツイッターアイコン">
+                  </a>
+                </div>
 		          	<div class="profile-area-image">
 		          			<img src="assets/img/profile-image.jpeg">
 		          	</div>
 
 		          	<div class="profile-area-description">
 		          		<p>
-		          			I'm a designer & front-end developer based in Tokyo, designing user-friendly interfaces with simplicity in mind for clients in Japan.
+		          			I'm a designer & front-end developer based in Tokyo, designing user-friendly interfaces with simplicity in mind for clients in Japan.<br><br>
 
-On the digital side, using HTML5, CSS3 and JavaScript I can create anything from a small static site, to a custom theme built for Wordpress. All projects are produced with SEO in mind making sure you have a great foundation to build on when it comes to being found in Google. I build sites that do not just look good on your desktop PC, but are also fully responsive so it looks and works great on all tablet devices and phones too.
+On the digital side, using HTML5, CSS3 and JavaScript I can create anything from a small static site, to a custom theme built for Wordpress. <br><br>All projects are produced with SEO in mind making sure you have a great foundation to build on when it comes to being found in Google. <br><br>I build sites that do not just look good on your desktop PC, but are also fully responsive so it looks and works great on all tablet devices and phones too.
 		          		</p>
 		          	</div>
 							</div>
