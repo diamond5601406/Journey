3fb7210189d295bd5120d0580e1f52e8ae6c4743
webdeveloperkei <jimbaran5601406@gmail.com>
2020-03-28 10:43:44 +0900

    add: Profileページ(PC)追加

---
+++ profile.php
@@ -0,0 +1,31 @@
+<?php require("./template/header.php"); ?>
+
+<div class="container-fluid">
+  <div class="row no-gutters">
+
+      <?php require("./template/navigation.php"); ?>
+
+      <div class="col-md-10 smp-home-main">
+      	<div class="main">
+      		<div class="row no-gutters">
+      			<div class="col-12 profile-area-wrapper">
+      				<div class="profile-area">
+      					<h1>PROFILE</h1>
+		          	<div class="profile-area-image">
+		          			<img src="assets/img/profile-image.jpeg">
+		          	</div>
+
+		          	<div class="profile-area-description">
+		          		<p>
+		          			I'm a designer & front-end developer based in Tokyo, designing user-friendly interfaces with simplicity in mind for clients in Japan.
+
+On the digital side, using HTML5, CSS3 and JavaScript I can create anything from a small static site, to a custom theme built for Wordpress. All projects are produced with SEO in mind making sure you have a great foundation to build on when it comes to being found in Google. I build sites that do not just look good on your desktop PC, but are also fully responsive so it looks and works great on all tablet devices and phones too.
+		          		</p>
+		          	</div>
+							</div>
+      			</div>
+      		</div>
+				</div>
+      </div>
+  </div>
+</div>
--- template/navigation.php
+++ template/navigation.php
@@ -13,8 +13,8 @@
         <!-- ハンバーガーメニュ―内部 768以上で非表示-->
         <ul class="smp-nav-list">
             <div class="smp-nav-btn-inactive">></div>
-            <li><a href="" class="home">Home</a></li>
-            <li><a href="#Profile" class="profile">Profile</a></li>
+            <li><a href="./index.php" class="home">Home</a></li>
+            <li><a href="./profile.php" class="profile">Profile</a></li>
             <li><a href="#Works" class="works">Works</a></li>
             <li><a href="#Gallery" class="gallery">Gallery</a></li>
             <li><a href="./contact.php" class="contact">Contact</a></li>
