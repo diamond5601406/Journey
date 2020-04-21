7b0e2f7c963971191acffdcb9793082418789efa
Kei Funatsuya <jimbaran5601406@gmail.com>
2020-04-21 16:31:46 +0900

    modified: Contact -> Blog

--- template/navigation.php
+++ template/navigation.php
@@ -7,8 +7,8 @@
             <li><a href="./index.php" class="home">Home</a></li>
             <li><a href="./profile.php" class="profile">Profile</a></li>
             <li><a href="./works.php" class="works">Works</a></li>
+            <li><a href="https://myqiita.com" class="contact">Blog</a></li>
             <li><button class="gallery">Gallery</button></li>
-            <li><a href="./contact.php" class="contact">Contact</a></li>
         </ul>
         <!-- ハンバーガーメニュ―内部 768以上で非表示-->
         <ul class="smp-nav-list">
@@ -16,8 +16,8 @@
             <li><a href="./index.php" class="home">Home</a></li>
             <li><a href="./profile.php" class="profile">Profile</a></li>
             <li><a href="./works.php" class="works">Works</a></li>
+            <li><a href="https://myqiita.com" class="contact">Blog</a></li>
             <li><button class="gallery">Gallery</button></li>
-            <li><a href="./contact.php" class="contact">Contact</a></li>
         </ul>
         <!-- ハンバーガーボタン(ハンバーガーメニュー起爆剤) 687以下で表示 -->
         <div class="smp-nav-trigger" href="#">
